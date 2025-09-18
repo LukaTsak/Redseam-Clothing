08106cd55c388f1ca0955b70ce6dcfdea2b69b68
lukaTsak <lukawaqadze01@gmail.com>
2025-09-18 23:05:45 +0400

    login page, headder and main photo

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { AuthPageComponent } from './auth-page/auth-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', redirectTo: 'login', pathMatch: 'full'},
+
+    {path: 'login', component: AuthPageComponent},
+];
