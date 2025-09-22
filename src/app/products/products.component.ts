58b7c1f0cbc751e27b6f01723a15e99a654093a7
lukaTsak <lukawaqadze01@gmail.com>
2025-09-22 13:17:35 +0400

    full pagination functionality and showing results made dynamic

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,8 +1,7 @@
 import { Component } from '@angular/core';
 import { ApiService } from '../services/api.service';
 import { CommonModule, NgForOf } from '@angular/common';
-import { ActivatedRoute, RouterModule } from '@angular/router';
-// import { CommonModule, NgForOf } from "../../../node_modules/@angular/common/common_module.d-NEF7UaHr";
+import { ActivatedRoute, Router, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-products',
@@ -11,25 +10,27 @@ import { ActivatedRoute, RouterModule } from '@angular/router';
   styleUrl: './products.component.scss',
 })
 export class ProductsComponent {
-  constructor(private apiService: ApiService, private route: ActivatedRoute) {}
+  constructor(
+    private apiService: ApiService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) {}
 
   user = JSON.parse(localStorage.getItem('user') || '{}');
   products: any = [];
   currentPage: number = 1;
-
-  dinamicPageNumber: number = 1;
+  dinamicPageNumber: number = 2;
 
   ngOnInit() {
     this.route.queryParams.subscribe((params) => {
       this.currentPage = params['page'] ? +params['page'] : 1;
-      this.dinamicPageNumber =
-        this.dinamicPageNumber < 8
-          ? this.dinamicPageNumber + 1
-          : this.dinamicPageNumber;
 
-          if(this.currentPage == 1){
-            this.dinamicPageNumber = 2;
-          }
+      this.dinamicPageNumber =
+        this.currentPage === 1
+          ? 2
+          : this.currentPage < 8
+          ? this.currentPage + 1
+          : this.dinamicPageNumber;
 
       console.log('Current page:', this.currentPage);
 
@@ -38,5 +39,30 @@ export class ProductsComponent {
         this.products = (res as any).data;
       });
     });
+  }
+
+  nextPage() {
+    this.router.navigate([], {
+      queryParams: { page: this.currentPage + 1 },
+      queryParamsHandling: 'merge',
+    });
+  }
+
+  prevPage() {
+    if (this.currentPage > 1) {
+      this.router.navigate([], {
+        queryParams: { page: this.currentPage - 1 },
+        queryParamsHandling: 'merge',
+      });
+    }
+  }
+
+  pageSize = 10;
+  totalItems = 100;
+
+  get resultsRange(): string {
+    const start = (this.currentPage - 1) * this.pageSize + 1;
+    const end = Math.min(this.currentPage * this.pageSize, this.totalItems);
+    return `${start}-${end}`;
   }
 }
