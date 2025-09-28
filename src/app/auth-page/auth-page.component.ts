ef230145864be30de45700056714379de62c86e4
lukaTsak <lukawaqadze01@gmail.com>
2025-09-28 18:11:30 +0400

    avatar at registration bug fix

--- src/app/auth-page/auth-page.component.ts
+++ src/app/auth-page/auth-page.component.ts
@@ -20,9 +20,9 @@ export class AuthPageComponent {
   token: any = '';
 
   ngOnInit() {
-    let token = localStorage.getItem('token')
-      // console.log('token exists' + token)
-    if ((token)) {
+    let token = localStorage.getItem('token');
+    // console.log('token exists' + token)
+    if (token) {
       this.router.navigate(['/products']);
       // console.log('token exists')
     }
@@ -49,6 +49,10 @@ export class AuthPageComponent {
   passwordType2 = 'password';
 
   incorrectPass: boolean = false;
+  incorrectUsername: boolean = false;
+  incorrectEmail: boolean = false;
+  passDontMatch: boolean = false;
+  passNotEnoughCaracters: boolean = false;
 
   makeVisible(x: number) {
     if (x === 1) {
@@ -60,9 +64,15 @@ export class AuthPageComponent {
     }
   }
 
-  resetIncorectPass() {
-    if (this.incorrectPass === true) {
+  resetIncorect(what: string) {
+    if (what === 'password') {
       this.incorrectPass = false;
+    } else if (what === 'passwordMatch') {
+      this.passDontMatch = false;
+    } else if (what === 'email') {
+      this.incorrectEmail = false;
+    } else if (what === 'username') {
+      this.incorrectUsername = false;
     }
   }
 
@@ -113,13 +123,17 @@ export class AuthPageComponent {
 
     if (this.password.length < 3) {
       this.showMessage('Password must be at least 3 characters long.');
+      this.passNotEnoughCaracters = true;
       return;
     }
 
     if (this.password !== this.confirmPassword) {
       this.showMessage('Passwords do not match.');
+      this.passDontMatch = true;
       return;
     }
+
+    this.passDontMatch = false;
 
     const formData = new FormData();
     formData.append('username', this.Username);
@@ -140,8 +154,19 @@ export class AuthPageComponent {
       },
       (err) => {
         console.error(err);
-        for (let el of Object.values(err.error.errors)) {
-          this.showMessage(el as string);
+        // this.showMessage(err)
+        for (let key in err.error.errors) {
+          if (
+            err.error.errors[key].includes('The email has already been taken.')
+          ) {
+            this.incorrectEmail = true;
+          } else if (
+            err.error.errors[key].includes(
+              'The username has already been taken.'
+            )
+          ) {
+            this.incorrectUsername = true;
+          }
         }
       }
     );
@@ -160,6 +185,12 @@ export class AuthPageComponent {
     this.userLogo = null;
 
     if (file) {
+      if (file.size > 1024 * 1024) {
+        // size in bytes (1 MB = 1024 * 1024 bytes)
+        this.showMessage('Image cannot be larger than 1 MB');
+        return;
+      }
+
       const reader = new FileReader();
       const image = new Image();
 
